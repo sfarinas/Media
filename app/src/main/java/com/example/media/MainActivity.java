5a8a889518200bd9e2a2badefc8fa78ebe0aa5c8
Salomao Farina <sfarinas@gmail.com>
2024-03-14 13:59:07 -0300

    Initial commit

---
+++ app/src/main/java/com/example/media/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.media;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivities(new Intent[]{new Intent(MainActivity.this, Home.class)});
+                finish();
+            }
+        }, 4000);
+    }
+}
